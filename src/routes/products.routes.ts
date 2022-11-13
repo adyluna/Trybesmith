f5a4fa016da06ee4c1a8cb65cc03f73d0fd6fe79
adyluna <adylunaleite@gmail.com>
2022-11-12 22:09:36 -0300

    Req 6 ok

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import ProductController from '../controller/product.controller';
+import ProductMiddleware from '../middlewares/product.middleware';
 
 const router = Router();
 
-router.post('/', ProductController.create);
+router.post(
+  '/',
+  ProductMiddleware.validateName,
+  ProductMiddleware.validateAmount,
+  ProductController.create,
+);
 router.get('/', ProductController.getAll);
 
 export default router;
